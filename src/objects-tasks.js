c37dd05bed6385a5cce03de101e4296bf8b980fe
artemgassan <artemgassan@ya.ru>
2025-03-01 02:39:43 +0300

    feat: finish sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -209,10 +209,17 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
-}
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country < b.country) return -1;
+    if (a.country > b.country) return 1;
+    if (a.city < b.city) return -1;
+    if (a.city > b.city) return 1;
 
+    return 0;
+  });
+}
+``;
 /**
  * Groups elements of the specified array by key.
  * Returns multimap of keys extracted from array elements via keySelector callback
